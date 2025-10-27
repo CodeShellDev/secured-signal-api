246bb6af01f13643931a7e0c8480c1972a18f479
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-10-27 19:29:15 +0100

    fix policy middleware order

--- internals/proxy/proxy.go
+++ internals/proxy/proxy.go
@@ -35,9 +35,9 @@ func (proxy Proxy) Init() http.Handler {
 		Use(m.Server).
 		Use(m.Auth).
 		Use(m.Endpoints).
-		Use(m.Policy).
 		Use(m.Template).
 		Use(m.Mapping).
+		Use(m.Policy).
 		Use(m.Message).
 		Then(proxy.Use())
 
