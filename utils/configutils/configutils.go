ff45896c13950e1f4bd3bfa2ce370b5d80dfe51f
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-10-28 20:30:49 +0100

    add default warning for when loadfunc is null

--- utils/configutils/configutils.go
+++ utils/configutils/configutils.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/codeshelldev/secured-signal-api/utils/jsonutils"
 	log "github.com/codeshelldev/secured-signal-api/utils/logger"
 	stringutils "github.com/codeshelldev/secured-signal-api/utils/stringutils"
 
@@ -27,6 +26,9 @@ type Config struct {
 func New() *Config {
 	return &Config{
 		Layer: koanf.New("."),
+		LoadFunc: func() {
+			log.Dev("Config.LoadFunc not initialized!")
+		},
 	}
 }
 
@@ -38,8 +40,6 @@ func (config *Config) LoadFile(path string, parser koanf.Parser) (koanf.Provider
 	log.Debug("Loading Config File: ", path)
 
 	f := file.Provider(path)
-
-	log.Info("*Config: ", jsonutils.ToJson(config))
 
 	err := config.Layer.Load(f, parser)
 
