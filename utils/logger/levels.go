55f1de0517ffcd265c1ca57434b4c119755a161c
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-11-07 19:07:05 +0100

    chore: restructure logger (#124)

--- utils/logger/levels.go
+++ utils/logger/levels.go
@@ -1,89 +1,51 @@
 package logger
 
-import (
-	"image/color"
-	"strconv"
-	"strings"
+import "go.uber.org/zap/zapcore"
 
-	"go.uber.org/zap/zapcore"
-)
+func Dev(data ...any) {
+	ok := logger.Check(DeveloperLevel, Format(data...))
 
-const DeveloperLevel zapcore.Level = -2
-
-func ParseLevel(s string) zapcore.Level {
-	switch strings.ToLower(s) {
-	case "dev":
-		return DeveloperLevel
-	case "debug":
-		return zapcore.DebugLevel
-	case "info":
-		return zapcore.InfoLevel
-	case "warn":
-		return zapcore.WarnLevel
-	case "error":
-		return zapcore.ErrorLevel
-	case "fatal":
-		return zapcore.FatalLevel
-	default:
-		return zapcore.InfoLevel
+	if ok != nil {
+		ok.Write()
 	}
 }
 
-func ColorCode(str string, color color.RGBA) string {
-	return startColor(color) + str + endColor()
+func Debug(data ...any) {
+	logger.Debug(Format(data...))
 }
 
-func ColorToInt(color color.RGBA) (int, int, int, int) {
-	r, g, b, a := color.R, color.G, color.B, color.A
-
-	red, green, blue, alpha := int(r), int(g), int(b), int(a)
-
-	return red, green, blue, alpha
+func Info(data ...any) {
+	logger.Info(Format(data...))
 }
 
-func startColor(color color.RGBA) string {
-	red, green, blue, alpha := ColorToInt(color)
-
-	mode := "38;2;"
-
-	if alpha >= 255 {
-		mode = "48;2;"
-	}
-
-	colorStr := strconv.Itoa(red) + ";" + strconv.Itoa(green) + ";" + strconv.Itoa(blue)
-
-	return "\x1b[" + mode + colorStr + "m"
+func Warn(data ...any) {
+	logger.Warn(Format(data...))
 }
 
-func endColor() string {
-	return "\x1b[0m"
+func Error(data ...any) {
+	logger.Error(Format(data...))
 }
 
-func LevelString(l zapcore.Level) string {
-	switch l {
-	case DeveloperLevel:
-		return "dev"
-	default:
-		return l.CapitalString()
-	}
+func Fatal(data ...any) {
+	logger.Fatal(Format(data...))
 }
 
-func CapitalLevel(l zapcore.Level) string {
-	switch l {
-	case DeveloperLevel:
-		return ColorCode("DEV  ", color.RGBA{
-			R: 95, G: 175, B: 135,
-		})
-	default:
-		return l.CapitalString()
-	}
+func IsDev() bool {
+	return logger.Level().Enabled(DeveloperLevel)
 }
 
-func CustomEncodeLevel(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
-	switch l {
-	case DeveloperLevel:
-		enc.AppendString(CapitalLevel(l))
-	default:
-		zapcore.CapitalColorLevelEncoder(l, enc)
-	}
+func IsDebug() bool {
+	return logger.Level().Enabled(zapcore.DebugLevel)
+}
+func IsInfo() bool {
+	return logger.Level().Enabled(zapcore.InfoLevel)
+}
+func IsWarn() bool {
+	return logger.Level().Enabled(zapcore.WarnLevel)
+}
+func IsError() bool {
+	return logger.Level().Enabled(zapcore.ErrorLevel)
+}
+func IsFatal() bool {
+	return logger.Level().Enabled(zapcore.FatalLevel)
 }
