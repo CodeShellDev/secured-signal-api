0aa10d96272e9779b89a8858c07b4957842f0dde
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2025-12-03 17:18:07 +0100

    bump gotl version for bugfix

--- internals/config/parser.go
+++ internals/config/parser.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"strings"
-
-	"github.com/codeshelldev/gotl/pkg/logger"
 )
 
 var transformFuncs = map[string]func(string, any) (string, any) {
@@ -14,16 +12,13 @@ var transformFuncs = map[string]func(string, any) (string, any) {
 }
 
 func keepTransform(key string, value any) (string, any) {
-	logger.Info(key)
 	return key, value
 }
 
 func lowercaseTransform(key string, value any) (string, any) {
-	logger.Info(key)
 	return strings.ToLower(key), value
 }
 
 func uppercaseTransform(key string, value any) (string, any) {
-	logger.Info(key)
 	return strings.ToUpper(key), value
 }
