d1fc88cb3f9a99e79e1e26225540ded8f5b8f5df
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2026-02-12 19:07:17 +0100

    changed indent for better view on docs

--- plugins/remark-prettier-ignore/index.js
+++ plugins/remark-prettier-ignore/index.js
@@ -4,10 +4,11 @@ export default function replacePrettierIgnore() {
 	return (tree) => {
 		visit(tree, "code", (node) => {
 			if (node.value) {
-				// Remove any line that contains "# prettier-ignore"
+				// Remove any line that contains "prettier-ignore"
 				node.value = node.value
 					.split("\n")
 					.filter((line) => !line.includes("# prettier-ignore"))
+					.filter((line) => !line.includes("// prettier-ignore"))
 					.join("\n")
 			}
 		})
