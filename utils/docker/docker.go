94809bfe0ec58096116ab858d4fb3a450b4aada0
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2026-03-30 21:24:27 +0200

    print buil time & last git commit at startup

--- utils/docker/docker.go
+++ utils/docker/docker.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/codeshelldev/gotl/pkg/docker"
@@ -18,68 +20,88 @@ var VERSION *semver.Version
 func Init() {
 	imageTag := os.Getenv("IMAGE_TAG")
 
-	if imageTag == "" {
-		return
-	}
+	if imageTag != "" {
+		if semver.IsValid(imageTag) {
+			v := semver.ParseSemver(imageTag)
 
-	if semver.IsValid(imageTag) {
-		v := semver.ParseSemver(imageTag)
+			VERSION = &v
 
-		VERSION = &v
+			logger.Info("Running ", VERSION.String(), " Image")
+			logger.Debug("Image built at ", )
 
-		logger.Info("Running ", VERSION.String(), " Image")
+			if VERSION.Type != semver.FULL_RELEASE {
+				box := pretty.NewAutoBox()
 
-		if VERSION.Type != semver.FULL_RELEASE {
-			box := pretty.NewAutoBox()
+				box.Border.Style.Color = pretty.Basic(pretty.BrightBlue)
 
-			box.Border.Style.Color = pretty.Basic(pretty.BrightBlue)
+				box.MinWidth = 60
+				box.PaddingX = 2
+				box.PaddingY = 1
 
-			box.MinWidth = 60
-			box.PaddingX = 2
-			box.PaddingY = 1
+				box.AddBlock(pretty.Block{
+					Align: pretty.AlignCenter,
+					Segments: []pretty.Segment{
+						pretty.TextBlockSegment{
+							Text: "🔬 Pre-Release 🔬",
+							Style: pretty.Style{
+								Bold: true,
+							},
+						},
+						pretty.InlineSegment{},
+					},
+				})
 
-			box.AddBlock(pretty.Block{
-				Align: pretty.AlignCenter,
-				Segments: []pretty.Segment{
-					pretty.TextBlockSegment{
-						Text: "🔬 Pre-Release 🔬",
-						Style: pretty.Style{
-							Bold: true,
+				box.AddBlock(pretty.Block{
+					Align: pretty.AlignCenter,
+					Segments: []pretty.Segment{
+						pretty.StyledTextBlockSegment{
+							Raw: "This is a" + 
+								func() string { if VERSION.Type == semver.ALPHA_RELEASE { return "n" } else { return "" } }() + 
+								" {i,b}" + string(VERSION.Type.Long()) + "{/}" + 
+								func() string { if VERSION.Type != semver.RC_RELEASE { return " release" } else { return "" } }() + ", it may contain {b,fg=red}bugs{/} and ",
+						},
+						pretty.StyledTextBlockSegment{
+							Raw: "some features may be {b,fg=bright_black}incomplete{/} or {b,fg=bright_yellow}unstable{/}",
 						},
 					},
-					pretty.InlineSegment{},
-				},
-			})
+				})
 
-			box.AddBlock(pretty.Block{
-				Align: pretty.AlignCenter,
-				Segments: []pretty.Segment{
-					pretty.StyledTextBlockSegment{
-						Raw: "This is a" + 
-							func() string { if VERSION.Type == semver.ALPHA_RELEASE { return "n" } else { return "" } }() + 
-							" {i,b}" + string(VERSION.Type.Long()) + "{/}" + 
-							func() string { if VERSION.Type != semver.RC_RELEASE { return " release" } else { return "" } }() + ", it may contain {b,fg=red}bugs{/} and ",
+				box.AddBlock(pretty.Block{
+					Align: pretty.AlignCenter,
+					Segments: []pretty.Segment{
+						pretty.InlineSegment{},
+						pretty.StyledTextBlockSegment{
+							Raw: "Encounter {b,fg=blue}issues{/}? Please {b,fg=blue}Report{/} them here:\n\n{b,u,fg=cyan}https://codeshelldev.github.io/secured-signal-api/bug?v=" + VERSION.String() + "{/}",
+						},
 					},
-					pretty.StyledTextBlockSegment{
-						Raw: "some features may be {b,fg=bright_black}incomplete{/} or {b,fg=bright_yellow}unstable{/}",
-					},
-				},
-			})
+				})
 
-			box.AddBlock(pretty.Block{
-				Align: pretty.AlignCenter,
-				Segments: []pretty.Segment{
-					pretty.InlineSegment{},
-					pretty.StyledTextBlockSegment{
-						Raw: "Encounter {b,fg=blue}issues{/}? Please {b,fg=blue}Report{/} them here:\n\n{b,u,fg=cyan}https://codeshelldev.github.io/secured-signal-api/bug?v=" + VERSION.String() + "{/}",
-					},
-				},
-			})
-
-			fmt.Println(box.Render())
+				fmt.Println(box.Render())
+			}
+		} else {
+			logger.Info("Running custom ", imageTag, " Image")
 		}
-	} else {
-		logger.Info("Running custom ", imageTag, " Image")
+	}
+	
+	buildTimestamp := os.Getenv("BUILD_TIME")
+	buildAt, err := strconv.Atoi(buildTimestamp)
+
+	if err == nil {
+		buildTime := time.Unix(int64(buildAt), 0)
+
+		if !buildTime.After(time.Now()) {
+			logger.Info("Built time: ", buildTime.Local().Format("01.02.06 15:04:05"))
+		}
+	}
+
+	commit := os.Getenv("GIT_COMMIT")
+
+	if commit != "" {
+		re, err := regexp.Compile(`\b[0-9a-f]{40}\b`)
+
+		if err == nil && re.MatchString(commit) {
+			logger.Info("Commit: ", commit)
+		}
 	}
 }
 
