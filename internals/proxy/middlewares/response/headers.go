f557773315caaabc73351b76026f751213a04b78
CodeShell <122738806+CodeShellDev@users.noreply.github.com>
2026-04-07 13:32:53 +0200

    make hardcoded response headers overwritable

--- internals/proxy/middlewares/response/headers.go
+++ internals/proxy/middlewares/response/headers.go
@@ -17,17 +17,17 @@ func headersHandler(res *http.Response) error {
 
 	resHeaders := conf.SETTINGS.HTTP.RESPONSE_HEADERS.OptOrEmpty(config.DEFAULT.SETTINGS.HTTP.RESPONSE_HEADERS)
 
-	if len(resHeaders) != 0 {
-		for k, v := range resHeaders {
-			res.Header.Set(k, v)
-		}
-	}
-
 	res.Header.Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, private, proxy-revalidate")
 	res.Header.Set("Pragma", "no-cache")
 	res.Header.Set("Expires", "0")
 	res.Header.Set("Vary", "*")
 	res.Header.Set("Referrer-Policy", "no-referrer")
+
+	if len(resHeaders) != 0 {
+		for k, v := range resHeaders {
+			res.Header.Set(k, v)
+		}
+	}
 
 	return nil
 }
